722f9a7728dd6652f44c29ff18731bebf32f7112
Scot Curry <scotcurry4@gmail.com>
2026-07-26 09:49:36 -0400

    Added weekly standings tables.

--- create_team_weekly_standings.sql
+++ create_team_weekly_standings.sql
@@ -1,10 +1,13 @@
 CREATE TABLE team_weekly_standings (
     team_key                    TEXT NOT NULL,
+    team_id                     INTEGER,
     week_key                    TEXT NOT NULL,
     rank                        INTEGER NOT NULL,
     points_for                  NUMERIC (10, 1),
     points_against              NUMERIC (10, 1),
     wins                        INTEGER,
     losses                      INTEGER,
+    ties                        INTEGER,
+    number_of_moves             INTEGER,
     PRIMARY KEY (team_key, week_key)    
 )
