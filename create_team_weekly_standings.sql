efea92a147e6f8f8f62cf7978757d6edcc2efac2
Scot Curry <scotcurry4@gmail.com>
2026-09-05 10:00:53 -0400

    Added user information table.

--- create_team_weekly_standings.sql
+++ create_team_weekly_standings.sql
@@ -1,7 +1,7 @@
 CREATE TABLE team_weekly_standings (
     team_key                    TEXT NOT NULL,
     team_id                     INTEGER,
-    week_key                    TEXT NOT NULL,
+    week_key                    INTEGER NOT NULL,
     rank                        INTEGER NOT NULL,
     points_for                  NUMERIC (10, 1),
     points_against              NUMERIC (10, 1),
---
+++ create_user_information.sql
@@ -0,0 +1,5 @@
+CREATE TABLE user_info (
+         user_id       INT      PRIMARY KEY ,
+         user_name     VARCHAR(80),
+         apns_token    VARCHAR(120)
+)
